e49c5b1f47f45167f5410f2b7a7327057cd466aa
QuierMathys <156114160+Quier-Mathys@users.noreply.github.com>
2025-11-24 12:56:11 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Définit la base des chemins en relatif pour supporter les sous-dossiers (ex: GitHub Pages)
+  base: '/NOM_DU_REPO_GITHUB/', // très important pour GitHub Pages
   build: {
     outDir: 'dist',
   },
   define: {
-    // Permet d'éviter le crash "process is not defined" dans le navigateur si on utilise process.env
     'process.env': {}
   }
 });
